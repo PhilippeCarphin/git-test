828d2347e42b1e33d62ed7c4e80ad8a5ebeabc5d
unknown <abc@123.com>
2018-01-27 19:10:18 -0500

    Corrected typo in print statement

--- helloworld.py
+++ helloworld.py
@@ -3,7 +3,7 @@
 # This is a big change, G star should pull before he continues
 print("This might be a conflict!")
 
-print("Hello WOrld!")
+print("Hello World!")
 # My comment is better than yours
 # This will cause a conflict but I pushed first so it's not my problem
 print("Git c'est vraiment le fun")
